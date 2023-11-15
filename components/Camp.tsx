851fd0b933aeb803312b2b5baa34cf9e6ec6afef
JuliaThTranNguyen <trhien541@gmail.com>
2023-11-15 13:14:47 +0200

    Marras 14th v.2.2

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -14,7 +14,7 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
       <div className="flexCenter gap-4">
         <div className="rounded-full bg-green-50 p-4">
-          <Image
+        <Image
             src="/folded-map.svg"
             alt="map"
             width={28}
